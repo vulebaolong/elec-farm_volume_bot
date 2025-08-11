dcbb0a5cf427a3084c229bbc58dc18c10bad166b
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-08-11 20:03:41 +0700

    1

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,18 +1,18 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import Provider from './components/provider/provider';
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import Provider from "./components/provider/provider";
 
-const container = document.getElementById('root') as HTMLElement;
+const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 root.render(
-  <Provider>
-    <App />
-  </Provider>,
+    <Provider>
+        <App />
+    </Provider>,
 );
 
 // calling IPC exposed from preload script
-window.electron?.ipcRenderer.once('ipc-example', (arg) => {
-  // eslint-disable-next-line no-console
-  console.log(arg);
+window.electron?.ipcRenderer.once("ipc-example", (arg) => {
+    // eslint-disable-next-line no-console
+    console.log(arg);
 });
-window.electron?.ipcRenderer.sendMessage('ipc-example', ['ping']);
+window.electron?.ipcRenderer.sendMessage("ipc-example", ["ping"]);
