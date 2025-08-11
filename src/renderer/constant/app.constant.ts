bbed8cb694e08ea32fe1be1e9222241e429d134e
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-08-11 18:28:35 +0700

    1

--- src/renderer/constant/app.constant.ts
+++ src/renderer/constant/app.constant.ts
@@ -1,5 +1,7 @@
-export const IS_PRODUCTION = false;
+// export const IS_PRODUCTION = false;
 // export const IS_PRODUCTION = true;
+export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
 
 export const BASE_URL = IS_PRODUCTION ? ` https://farmvol.feliciastation.com/api/` : ` http://localhost:3069/api/`;
 export const BASE_DOMAIN = IS_PRODUCTION ? `https://farmvol.feliciastation.com/` : `http://localhost:3069/`;
