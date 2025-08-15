8b296c5bbaa29346e16dae650658ad8619e72f55
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-08-15 18:09:33 +0700

    1

--- doc.js
+++ doc.js
@@ -2,8 +2,8 @@
 git tag -f v1.0.0             
 git push origin v1.0.0 --force
 
-git tag v1.0.7     
-git push origin v1.0.7
+git tag v1.0.8     
+git push origin v1.0.8
 
 xattr -cr "/Applications/ElectronReact.app"
 
