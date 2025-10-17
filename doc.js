bac7967ee87873544b315a23517fea333b567f90
VU LE BAO LONG <87232600+vulebaolong@users.noreply.github.com>
2025-10-17 14:37:56 +0700

    1

--- doc.js
+++ doc.js
@@ -1,11 +1,11 @@
 /**
-git tag -f v1.0.77             
-git push origin v1.0.77 --force
+git tag -f v1.0.78             
+git push origin v1.0.78 --force
 
-git tag v1.0.77
-git push origin v1.0.77
+git tag v1.0.78
+git push origin v1.0.78
 
-xattr -cr "/Applications/BotFarmVolume_v1_0_77.app"
+xattr -cr "/Applications/BotFarmVolume_v1_0_78.app"
 
  */
 
